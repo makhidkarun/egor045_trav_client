80efb4adaa1b80129a1d609accf8f1dd4fe2fa60
Arthur Green <arthur@phraction.org>
2018-04-11 16:35:44 +0100

    Add api_doc.py, api_doc.html - retrieve and display API documentation

---
+++ web/app/main/api_doc.py
@@ -0,0 +1,51 @@
+'''api_doc.py'''
+
+import requests
+from flask import current_app, request, render_template
+from . import main
+
+
+@main.route('/api_doc', methods=['GET'])
+def api_doc():
+    '''Render API documentation for endpoint (endpoint must be in routes)'''
+
+    data = {}
+    error_msg = None
+    navbar_items = []
+    label = 'Unknown'
+
+    endpoint = request.args.get('endpoint')
+    label = request.args.get('label')
+    if label is None:
+        label = endpoint
+    current_app.logger.debug('endpoint = {}'.format(endpoint))
+    current_app.logger.debug('label = {}'.format(label))
+    if endpoint in current_app.config['KNOWN_API_ENDPOINTS']:
+        navbar_items = current_app.config['KNOWN_API_ENDPOINTS'][endpoint]
+        # Get API doc
+        try:
+            resp = requests.get(
+                '{}/{}?doc=true'.format(
+                    current_app.config['API_SERVER'],
+                    endpoint
+                )
+            )
+            if resp.status_code == 200:
+                data = resp.json()
+            else:
+                error_msg = 'Received status {} from API endpoint'.format(
+                    resp.status_code)
+                current_app.logger.debug(error_msg)
+        except requests.ConnectionError:
+            current_app.logger.debug('Unable to connect to API endpoint')
+            error_msg = 'Unable to connect to API server'
+
+        current_app.logger.debug('data = %s', data)
+
+    return render_template(
+        'api_doc.html',
+        data=data,
+        error_msg=error_msg,
+        navbar_items=navbar_items,
+        label=label
+    )
